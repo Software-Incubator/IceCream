3da746ba5718a270a927c00a3bc84ea97c065a4f
Animesh Kumar <animesh0721@gmail.com>
2016-07-31 09:32:45 +0530

    Improves form layout
    * adds spacing b/w fields of registration form
    * adds timestamp to Registration model

--- Core/admin.py
+++ Core/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from models import Technology, Member, Designation, Project, ContactUs, Registration
 
+
+class RegistrationAdmin(admin.ModelAdmin):
+    readonly_fields = ('timestamp', )
+
+
 admin.site.register(Technology)
 admin.site.register(Member)
 admin.site.register(Designation)
 admin.site.register(Project)
 admin.site.register(ContactUs)
-admin.site.register(Registration)
+admin.site.register(Registration, RegistrationAdmin)
--- Core/models.py
+++ Core/models.py
@@ -94,6 +94,7 @@ class Registration(models.Model):
     gender = models.CharField(max_length=1)
     hosteler = models.BooleanField(default=False)
     designer = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return "{} || {} || {}".format(self.name, self.branch, str(self.year))
