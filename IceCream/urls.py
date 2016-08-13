ba56d2ce2f348625999493534811277d4c6a3371
Bansal Utkarsh <bansalutkarsh3@gmail.com>
2016-08-13 22:29:49 +0530

    Adds redirect for bytepad

--- IceCream/urls.py
+++ IceCream/urls.py
@@ -19,10 +19,12 @@ from django.conf import settings
 from django.contrib import admin
 # from Core.views import Contact_Form
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('Core.urls')),
+    url(r'^bytepad/', RedirectView.as_view(url='http://bytepad.silive.in'))
     # url(r'^form/', Contact_Form, name="form" )
 ]
 
