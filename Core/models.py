0c122ef1843461aa5bc88f3000c3e8b77cd86364
Bansal Utkarsh <bansalutkarsh3@gmail.com>
2016-08-13 22:18:48 +0530

    Fix media upload paths

--- Core/models.py
+++ Core/models.py
@@ -1,9 +1,28 @@
 from __future__ import unicode_literals
+
+import os
+import uuid
+
 from django.db import models
 
 
+## DO NOT DELETE THIS FUNCTION
+## If you think that this function is outright useless because it is never called,
+## stop right there. You delete this and all the migrations will FAIL!
 def upload_location(instance, filename):
-    return '%s/%s' %(instance.id, filename)
+    return '%s/%s' % (instance.id, filename)
+
+
+def members_upload_location(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = "%s.%s" % (uuid.uuid4(), ext)
+    return os.path.join('uploads/members', filename)
+
+
+def projects_upload_location(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = "%s.%s" % (uuid.uuid4(), ext)
+    return os.path.join('uploads/projects', filename)
 
 
 class ContactUs(models.Model):
@@ -12,7 +31,6 @@ class ContactUs(models.Model):
     email = models.EmailField(max_length=50)
     subject = models.CharField(max_length=255)
     message = models.CharField(max_length=500)
-
 
     def __str__(self):
         return self.name
@@ -49,11 +67,11 @@ class Member(models.Model):
     presently_at = models.CharField(max_length=225, null=False, blank=True)
     is_alumni = models.BooleanField(default=False)
     batch = models.IntegerField(null=False)
-    pic_path = models.FileField(upload_to=upload_location, null=True, default=None)
+    pic_path = models.FileField(upload_to=members_upload_location, null=True,
+                                default=None)
     facebook = models.CharField(max_length=225, null=True, default=None)
     github = models.CharField(max_length=225, null=True, default=None)
     linkedin = models.CharField(max_length=225, null=True, default=None)
-
 
     def __str__(self):
         return self.name + " | " + str(
@@ -68,7 +86,8 @@ class Project(models.Model):
     name = models.CharField(max_length=225, null=False, blank=False)
     description = models.CharField(max_length=225, null=False, blank=False)
     completion_year = models.IntegerField(null=True, default=None)
-    image_path = models.FileField(upload_to=upload_location, null=True, default=None)
+    image_path = models.FileField(upload_to=projects_upload_location, null=True,
+                                  default=None)
 
     def __str__(self):
         return self.name + str(self.completion_year)
@@ -98,5 +117,3 @@ class Registration(models.Model):
 
     def __str__(self):
         return "{} || {} || {}".format(self.name, self.branch, str(self.year))
-
-
