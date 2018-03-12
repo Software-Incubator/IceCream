d26e4136e03c83cc75f0980039a0c1247cabb535
Apoorva Pandey <apoorvapandey365@gmail.com>
2018-03-12 21:21:57 +0530

    Add excel download for registrations

--- IceCream/settings.py
+++ IceCream/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Core',
-    'ckeditor'
+    'ckeditor',
+    'import_export'
 ]
 
 MIDDLEWARE_CLASSES = [
